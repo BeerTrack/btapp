1d0170ce2de0bb1e00e3a6a205d0b8b68049ad0b
Phil Everson <phil@phileverson.com>
2015-02-21 15:25:35 -0500

    Navigation updated with actual working links to the correct sections (which show the basic framework at the moment) and fixed a bunch of index pages.

--- app/_shared/_header.php
+++ app/_shared/_header.php
@@ -21,10 +21,29 @@ ob_start();
         <link href="../../../assets/css/AdminLTE.css" rel="stylesheet" type="text/css" />
         <link href="../../../assets/css/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
 
-
         <!-- Start Copy Footer -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="//code.jquery.com/ui/1.11.1/jquery-ui.min.js" type="text/javascript"></script>
+         <!-- Morris.js charts -->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+        <script src="../../../assets/js/plugins/morris/morris.min.js" type="text/javascript"></script>
+        <!-- Sparkline -->
+        <script src="../../../assets/js/plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
+        <!-- jvectormap -->
+        <script src="../../../assets/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script>
+        <script src="../../../assets/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
+        <!-- jQuery Knob Chart -->
+        <script src="../../../assets/js/plugins/jqueryKnob/jquery.knob.js" type="text/javascript"></script>
+        <!-- daterangepicker -->
+        <script src="../../../assets/js/plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
+        <!-- datepicker -->
+        <script src="../../../assets/js/plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>
+        <!-- Bootstrap WYSIHTML5 -->    
+        <script src="../../../assets/js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js" type="text/javascript"></script>
+        <!-- iCheck -->
+        <script src="../../../assets/js/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+
         <!-- DATA TABES SCRIPT -->
         <script src="../../../assets/js/plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
         <script src="../../../assets/js/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
@@ -32,6 +51,7 @@ ob_start();
         <script src="../../../assets/js/AdminLTE/app.js" type="text/javascript"></script>
         <!-- AdminLTE for demo purposes -->
         <script src="../../../assets/js/AdminLTE/demo.js" type="text/javascript"></script>
+        <script src="../../../assets/js/AdminLTE/dashboard.js" type="text/javascript"></script>
         <!-- End Copy Footer -->
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -44,7 +64,7 @@ ob_start();
     <body class="skin-blue">
         <!-- header logo: style can be found in header.less -->
         <header class="header">
-            <a href="../index.html" class="logo">
+            <a href="/" class="logo">
                 <!-- Add the class icon to your logo image or logo icon to add the margining -->
                 AdminLTE
             </a>
---
+++ app/global/brewery-details/_updateDetails.php
@@ -0,0 +1,55 @@
+<!-- general form elements disabled -->
+                            <div class="box box-primary">
+                                <div class="box-header">
+                                    <h3 class="box-title">Brewery Details</h3>
+                                </div><!-- /.box-header -->
+                                <div class="box-body">
+                                    <form role="form" method="post" action="?requestedAction=add">
+                                        <!-- text input -->
+                                        <div class="form-group">
+                                            <labelfor="beerName">Name</label>
+                                            <input type="text" class="form-control" id="beerName" name="beerName" placeholder="Enter Name"/>
+                                        </div>
+
+                                         <div class="form-group">
+                                            <labelfor="beerPrice">Price</label>
+                                            <input type="number" class="form-control" id="beerPrice" name="beerPrice" placeholder="Enter Price"/>
+                                        </div>
+
+                                         <div class="form-group">
+                                            <labelfor="beerSize">Size</label>
+                                            <input type="number" class="form-control" id="beerSize" name="beerSize" placeholder="Enter Size in ml"/>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <labelfor="beerType">Select Type</label>
+                                            <select class="form-control" id="beerType" name="beerType">
+                                                <option>Bottle</option>
+                                                <option>Can</option>
+                                            </select>
+                                        </div>
+
+                                         <div class="form-group">
+                                            <labelfor="beerQuantity">Select Quantity</label>
+                                            <select class="form-control" id="beerQuantity" name="beerQuantity">
+                                                <option>1</option>
+                                                <option>6</option>
+                                                <option>12</option>
+                                                <option>24</option>
+                                            </select>
+                                        </div>
+                                   
+                                        <div class="box-footer">
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                        </div>
+
+                                    </form>
+                                </div><!-- /.box-body -->
+                            </div><!-- /.box -->
+<?php
+//HTML and any necessary PHP for adding a beer.
+?>
+
+<!-- - Form with fields for adding a beer (See elements here: http://almsaeedstudio.com/AdminLTE/pages/forms/general.html)</br>
+- POST this data back to the index page </br>
+- finish "addBeer" function (in index.php), it should submit the data back to the database.  -->
