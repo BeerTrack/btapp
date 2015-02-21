a02192960347baf5656cfe099842066c5e69aba3
sitesh <tesh@v1022-rn-56-109.campus-dynamic.uwaterloo.ca>
2015-02-21 12:28:32 -0500

    Created form for add beer page, created insert beer statement into database. Not tested!

--- app/global/manage-beers/_addBeer.php
+++ app/global/manage-beers/_addBeer.php
@@ -1,7 +1,50 @@
+<!-- general form elements disabled -->
+                            <div class="box box-primary">
+                                <div class="box-header">
+                                    <h3 class="box-title">Add A New Beer</h3>
+                                </div><!-- /.box-header -->
+                                <div class="box-body">
+                                    <form role="form" method="post" action="?requestedAction=add">
+                                        <!-- text input -->
+                                        <div class="form-group">
+                                            <labelfor="beerName">Name</label>
+                                            <input type="text" class="form-control" id="beerName" name="beerName" placeholder="Enter Name"/>
+                                        </div>
+
+                                         <div class="form-group">
+                                            <labelfor="beerPrice">Price</label>
+                                            <input type="number" class="form-control" id="beerPrice" name="beerPrice" placeholder="Enter Price"/>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <labelfor="beerType">Select Type</label>
+                                            <select class="form-control" id="beerType" name="beerType">
+                                                <option>Bottle</option>
+                                                <option>Can</option>
+                                            </select>
+                                        </div>
+
+                                         <div class="form-group">
+                                            <labelfor="beerSize">Select Size</label>
+                                            <select class="form-control" id="beerSize" name="beerSize">
+                                                <option>1</option>
+                                                <option>6</option>
+                                                <option>12</option>
+                                                <option>24</option>
+                                            </select>
+                                        </div>
+                                   
+                                        <div class="box-footer">
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                        </div>
+
+                                    </form>
+                                </div><!-- /.box-body -->
+                            </div><!-- /.box -->
 <?php
 //HTML and any necessary PHP for adding a beer.
 ?>
 
-- Form with fields for adding a beer (See elements here: http://almsaeedstudio.com/AdminLTE/pages/forms/general.html)</br>
+<!-- - Form with fields for adding a beer (See elements here: http://almsaeedstudio.com/AdminLTE/pages/forms/general.html)</br>
 - POST this data back to the index page </br>
-- finish "addBeer" function (in index.php), it should submit the data back to the database. 
+- finish "addBeer" function (in index.php), it should submit the data back to the database.  -->
