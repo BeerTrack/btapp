547317f3cd3deb9698f71cd40a22be58c0b2fe27
Phil Everson <phil@phileverson.com>
2015-03-18 16:54:34 -0400

    More work on the shipment planner. Now produces a route using google maps templated API thing. Not done, my brain really hurts. Because of all the background processes this is a hard UX to create/define.

--- app/decide/shipment-planner/_makePlan.php
+++ app/decide/shipment-planner/_makePlan.php
@@ -1,176 +1,152 @@
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAfsoOmZeHXm5M4O_fMZWb9yL0VJBP6ZCU"></script>
+<!-- <script src="gmaps_api.js"></script> -->
+<script src="custom_phil.js"></script>
+
 <div class="row">
-  <div class="col-xs-12">
-    <div class="box box-primary">
-      <div class="box-header">
-        <h3 class="box-title">Create Shipping Manifest</h3>
-      </div>
-      <div class="box-body">
-        <table id="allStoresTable" class="table table-hover table-bordered">
-          <thead>
-            <tr style="background-color: #ECECEC">
-              <th>Beer</th>
-              <th>Package Type</th>
-              <th>Unit Volume</th>
-              <th>Quanity per Package</th>
-              <th>Current Inventory Level</th>
-              <th>Quanity to Ship</th>
-            </tr>
-          </thead>
-          <tbody>
-          <?php
-          function rowCreatorPerStore($loggedInBreweryID, $passedBeerStoreID, $storeNamePrint)
-          {
-            $possibleBeersQuery = beerTrackDBQuery(
-              "SELECT s.location_name, bb.beer_name, ipd.single_package_type, ipd.single_package_volume, ipd.single_package_quantity, ipd.stock_at_timestamp
-              FROM stores s, beer_brands bb, inventory_parsing ipd
-              WHERE 
-              s.brewery_id = '$loggedInBreweryID' AND
-              s.beerstore_store_id = '$passedBeerStoreID' AND
-              ipd.brewery_id = s.brewery_id AND
-              ipd.beerstore_beer_id = bb.beerstore_beer_id AND
-              ipd.beerstore_store_id = s.beerstore_store_id
-              ORDER BY s.location_name");
+	<div class="col-xs-12">
+		<div class="panel box box-primary">
+			<div class="box-header with-border">
+				<h4 class="box-title">
+					<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" class="" aria-expanded="true">
+						Shipping Manifest
+					</a>
+				</h4>
+			</div>
+			<div id="collapseOne" class="panel-collapse collapse in" aria-expanded="false">
+				<div class="box-body">
+					<table id="allStoresTable" class="table table-hover table-bordered">
+						<thead>
+							<tr style="background-color: #ECECEC">
+								<th>Beer</th>
+								<th>Package Type</th>
+								<th>Unit Volume</th>
+								<th>Quanity per Package</th>
+								<th>Current Inventory Level</th>
+								<th>Quanity to Ship</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+							function rowCreatorPerStore($loggedInBreweryID, $passedBeerStoreID, $storeNamePrint)
+							{
+								$possibleBeersQuery = beerTrackDBQuery(
+									"SELECT s.location_name, bb.beer_name, ipd.single_package_type, ipd.single_package_volume, ipd.single_package_quantity, ipd.stock_at_timestamp
+									FROM stores s, beer_brands bb, inventory_parsing ipd
+									WHERE 
+									s.brewery_id = '$loggedInBreweryID' AND
+									s.beerstore_store_id = '$passedBeerStoreID' AND
+									ipd.brewery_id = s.brewery_id AND
+									ipd.beerstore_beer_id = bb.beerstore_beer_id AND
+									ipd.beerstore_store_id = s.beerstore_store_id
+									ORDER BY s.location_name");
 
-            echo '<tr style="background-color: #ECECEC"><td colspan="6"><strong>' . $storeNamePrint . '</strong>';
-            // echo '<td colspan="1"><div class="form-group" style="margin-bottom:0px; float: right"><label style="margin-bottom:0px;"class="">Include in Shipment Plan<div style="display:inline; margin-left: 10px;   top: -2px;  position: relative;" class="includeStoreCheck icheckbox_minimal-blue checked" aria-checked="true" aria-disabled="false" style="position: relative;"><input type="checkbox" onClick="alert(\'Hello from JavaScript!\')" name="" id="test" class="minimal" checked="" style="position: absolute; opacity: 0;"></div></label></div></td></tr>';
-            // echo '<td colspan="1"><label style="margin-bottom:0px;"class="">Include in Shipment Plan<input type="checkbox" onClick="alert(\'Hello from JavaScript!\')" name="" class="checkbox" checked="" style="position: absolute; opacity: 0;"></label></div></td></tr>';
-            echo '
-            <div class="form-group" style="margin-bottom: 0px; float: right">
-                <select id="decider_' . $passedBeerStoreID  . '" onChange="includeStoreCheck(\'' . $passedBeerStoreID . '\')" name="inventory_location" class="includeStoreCheck form-control">
-                    <option value="include">Include in Shipment Plan</option>
-                    <option value="exclude">Exclude from Shipment Plan</option>
-                </select>
-            </div>
-            </tr>';
+								echo '<tr style="background-color: #ECECEC"><td colspan="6"><strong>' . $storeNamePrint . '</strong>';
+								echo '
+								<div class="form-group" style="margin-bottom: 0px; float: right">
+									<select id="decider_' . $passedBeerStoreID  . '" onChange="includeStoreCheck(\'' . $passedBeerStoreID . '\')" name="inventory_location" class="includeStoreCheck form-control">
+										<option value="include">Include in Shipment Plan</option>
+										<option value="exclude">Exclude from Shipment Plan</option>
+									</select>
+								</div>
+							</tr>';
 
 
-            while($row = mysqli_fetch_array($possibleBeersQuery)) {
-              // print_r($row);
-              echo "<tr class=\"items_" . $passedBeerStoreID . "\">";
-              echo "<td>" . $row['beer_name'] . "</td>";
-              echo "<td>" . $row['single_package_type'] . "</td>";
-              echo "<td>" . $row['single_package_volume'] . " ml</td>";
-              echo "<td>" . $row['single_package_quantity'] . "</td>";
-              echo "<td>" . $row['stock_at_timestamp'] . "</td>";
-              echo "<td><input style=\"float:right; width:100%\" type=\"text\"></td>";
-              echo "</tr>";
-            }
-          }
+							while($row = mysqli_fetch_array($possibleBeersQuery)) {
+								echo "<tr class=\"items_" . $passedBeerStoreID . "\">";
+								echo "<td>" . $row['beer_name'] . "</td>";
+								echo "<td>" . $row['single_package_type'] . "</td>";
+								echo "<td>" . $row['single_package_volume'] . " ml</td>";
+								echo "<td>" . $row['single_package_quantity'] . "</td>";
+								echo "<td>" . $row['stock_at_timestamp'] . "</td>";
+								echo "<td><input style=\"float:right; width:100%\" type=\"text\"></td>";
+								echo "</tr>";
+							}
+						}
 
-          $possibleStoresQuery = beerTrackDBQuery("SELECT * FROM stores WHERE brewery_id = '$loggedInBreweryID'"); 
-          while($singleStore = mysqli_fetch_array($possibleStoresQuery)) {
-            rowCreatorPerStore($loggedInBreweryID, $singleStore['beerstore_store_id'], $singleStore['location_name']);
-          }
-          ?>
-          </tbody>
+						$gMapsStoreListing = '';
+						$gMapsStoreListingList = '';
+						$possibleStoresQuery = beerTrackDBQuery("SELECT * FROM stores WHERE brewery_id = '$loggedInBreweryID'"); 
+						while($singleStore = mysqli_fetch_array($possibleStoresQuery)) {
+							rowCreatorPerStore($loggedInBreweryID, $singleStore['beerstore_store_id'], $singleStore['location_name']);
+							$gMapsStoreListing .= '<option class="storeSelectOption" id="storeID_' . $singleStore['beerstore_store_id'] . '" value="' . $singleStore['location_address']  . '"> ' . $singleStore['location_name'] . '</option>';
+							$gMapsStoreListingList .= '<p style="margin-bottom:0px" id="storeIDList_' . $singleStore['beerstore_store_id'] . '"> ' . $singleStore['location_name'] . '</p>';
+						}
+						?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class="panel box box-primary">
+			<div class="box-header with-border">
+				<h4 class="box-title">
+					<a id="collapseDeliveryToggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" class="collapsed" aria-expanded="false">
+						Delivery Route
+					</a>
+				</h4>
+			</div>
+			<div id="collapseTwo" style="width: 100%" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
+				<div class="box-body col-xs-12" >
+					<div id="map-canvas" class="col-xs-8" style=""></div>
 
-        </table>
-      </div>
-    </div>
-  </div>
+					<div id="control_panel" class="col-xs-4" style="float:right;">
+						<div>
+							<b>Start:</b>
+							<p id="start">200 University Ave. West, Waterloo, ON</p>
+							<b>Stores on Route:</b> <br>
+								<?php 
+									echo $gMapsStoreListingList;
+								?>
+							<p></p>
+							<select class="gmapsStoreListing" multiple id="waypoints" style="display:none">
+								<?php
+									echo $gMapsStoreListing;
+								?>
+							</select>
+							<b>End:</b>
+							<p id="end">200 University Ave. West, Waterloo, ON</p>
+							<!-- <input type="submit" onclick="calcRoute();"> -->
+						</div>
+						<div id="directions_panel" style="background-color:#FFEE77;"></div>
+					</div> 	
+				</div>
+			</div>
+		</div>
+		<div class="panel box box-primary">
+			<div class="box-header with-border">
+				<h4 class="box-title">
+					<a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" class="collapsed" aria-expanded="false">
+						Finalize & Distribute Plan
+					</a>
+				</h4>
+			</div>
+			<div id="collapseThree" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
+				<div class="box-body">
+					Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
-<script type="text/javascript">
-//Hide all rows for a given store
-
-// $('.icheckbox_minimal').click(function() {
-//   console.log('clicked'); 
-//     if(this.attr("aria-checked") == "true")
-//     {
-//         //Do stuff
-//         alert('test - true');
-//     }
-//     else 
-//     {
-//       alert(this.attr("id"))
-//     }
-// });
-
-// $(".includeStoreCheck").change(function() {
-//   console.log(this.val());
-
-//     // if(this.attr("value") == "include")
-//     // {
-//     //     alert('test - include');
-//     // }
-//     // else 
-//     // {
-//     //   alert('test - exclude');
-//     // }
-// });
-
-function includeStoreCheck (className) {
-  var decision = $("#decider_" + className ).val();
-  if(decision === "include")
-  {
-    $(".items_" + className).each(function( index ) {
-      $( this ).css("display", "table-row");
-    });
-  }
-  if(decision === "exclude")
-  {
-    $(".items_" + className).each(function( index ) {
-      $( this ).css("display", "none");
-    });
-  }
-}
+<style>
+	#map-canvas {
+		height: 100%;
+		margin: 0px;
+		padding: 0px
+	}
+	#panel {
+		position: absolute;
+		top: 5px;
+		left: 50%;
+		margin-left: -180px;
+		z-index: 5;
+		background-color: #fff;
+		padding: 5px;
+		border: 1px solid #999;
+	}
+</style>
 
 
-</script>
-
-    <style>
-      html, body, #map-canvas {
-        height: 100%;
-        margin: 0px;
-        padding: 0px
-      }
-      #panel {
-        position: absolute;
-        top: 5px;
-        left: 50%;
-        margin-left: -180px;
-        z-index: 5;
-        background-color: #fff;
-        padding: 5px;
-        border: 1px solid #999;
-      }
-    </style>
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAfsoOmZeHXm5M4O_fMZWb9yL0VJBP6ZCU"></script>
-    <script src="gmaps_api.js"></script>
 
 
-    <div id="map-canvas" style="float:left;width:70%;height:100%;"></div>
-    <div id="control_panel" style="float:right;width:30%;text-align:left;padding-top:20px">
-    <div style="margin:20px;border-width:2px;">
-    <b>Start:</b>
-    <select id="start">
-      <option value="Halifax, NS">Halifax, NS</option>
-      <option value="Boston, MA">Boston, MA</option>
-      <option value="New York, NY">New York, NY</option>
-      <option value="Miami, FL">Miami, FL</option>
-    </select>
-    <br>
-    <b>Waypoints:</b> <br>
-    <i>(Ctrl-Click for multiple selection)</i> <br>
-    <select multiple id="waypoints">
-      <option value="montreal, quebec">Montreal, QBC</option>
-      <option value="toronto, ont">Toronto, ONT</option>
-      <option value="chicago, il">Chicago</option>
-      <option value="winnipeg, mb">Winnipeg</option>
-      <option value="fargo, nd">Fargo</option>
-      <option value="calgary, ab">Calgary</option>
-      <option value="spokane, wa">Spokane</option>
-    </select>
-    <br>
-    <b>End:</b>
-    <select id="end">
-      <option value="Vancouver, BC">Vancouver, BC</option>
-      <option value="Seattle, WA">Seattle, WA</option>
-      <option value="San Francisco, CA">San Francisco, CA</option>
-      <option value="Los Angeles, CA">Los Angeles, CA</option>
-    </select>
-    <br>
-      <input type="submit" onclick="calcRoute();">
-    </div>
-    <div id="directions_panel" style="margin:20px;background-color:#FFEE77;"></div>
-    </div>
---
+++ app/decide/shipment-planner/custom_phil.js
@@ -0,0 +1,110 @@
+//copied from GMAPS:
+
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+function initialize() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var canada = new google.maps.LatLng(45.4000, -75.6667);
+  var mapOptions = {
+    zoom: 10,
+    center: canada,
+    disableDefaultUI: true
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  directionsDisplay.setMap(map);
+
+
+}
+
+function calcRoute() {
+  var start = document.getElementById('start').innerHTML;
+  var end = document.getElementById('end').innerHTML;
+  var waypts = [];
+  var checkboxArray = document.getElementById('waypoints');
+  for (var i = 0; i < checkboxArray.length; i++) {
+    if (checkboxArray.options[i].selected == true) {
+      waypts.push({
+          location:checkboxArray[i].value,
+          stopover:true});
+    }
+  }
+
+  var request = {
+      origin: start,
+      destination: end,
+      waypoints: waypts,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+      var route = response.routes[0];
+      var summaryPanel = document.getElementById('directions_panel');
+      summaryPanel.innerHTML = '';
+      // For each route, display summary information.
+      for (var i = 0; i < route.legs.length; i++) {
+        var routeSegment = i + 1;
+        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment + '</b><br>';
+        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+      }
+    }
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+//Phil's stuff:
+
+function includeStoreCheck (className) {
+	var decision = $("#decider_" + className ).val();
+	if(decision === "include")
+	{
+		$(".items_" + className).each(function( index ) {
+			$( this ).css("display", "table-row");
+		});
+		//selecting the item in the google maps world
+		$("#storeID_"  + className ).removeClass('notpicked');
+		$("#storeIDList_"  + className ).css("display", "block");
+	}
+	if(decision === "exclude")
+	{
+		$(".items_" + className).each(function( index ) {
+			$( this ).css("display", "none");
+			$("#storeID_"  + className ).addClass('notpicked');
+			$("#storeIDList_"  + className ).css("display", "none");
+		});
+	}
+}
+
+
+$( document ).ready(function() {
+	$("#collapseDeliveryToggle").click(function(){
+		$("#collapseTwo").css('display', 'inline-block');
+		initialize(); //resetting google map
+
+		//clears them all
+		$("#waypoints option:selected").removeAttr("selected");
+
+		//re selects the right ones...
+		$(".storeSelectOption").each(function( index ) {
+			if($( this ).hasClass('notpicked'))
+			{
+				$( this ).removeAttr("selected");
+			}
+			else
+			{
+				$( this ).attr('selected', 'selected');
+			}
+		});
+
+		setTimeout(function(){calcRoute(); console.log('ran calcRoute');},500);
+
+	})
+})
