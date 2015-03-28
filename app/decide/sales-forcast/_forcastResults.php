7b41416ac44d902c2fd22db70f5d4fde10c6f310
ChristianGould <christian-gould@hotmail.com>
2015-03-27 20:50:48 -0400

    Cleaned up forecast input menu, modified individual forecast function to handle empty data sets Started trying to implement production planning, currently have predicted total volume across all stores for a single beer, quantity, volume, and package type combination Whenever I change the quantity, package type, or volume the code breaks

--- app/decide/sales-forcast/_forcastResults.php
+++ app/decide/sales-forcast/_forcastResults.php
@@ -1,9 +1,23 @@
 <?php
 
 echo '<h2>hi</h2>';
-echo ' The forecasted number of sales is: ' . singleDayForecastGen($textDateRange, $beerID, $storeID, $container, $quantity, $volume);
+//echo ' The forecasted number of sales is: ' . singleDayForecastGen($textDateRange, $beerID, $storeID, $container, $quantity, $volume);
 
+//getting names of the beers associated with this brewery
+$stores = beerTrackDBQuery("SELECT DISTINCT beerstore_store_id, single_package_type, single_package_quantity, single_package_volume
+FROM inventory_parsing
+WHERE beerstore_beer_id = 3066");
 
+$total = 0;
+while($row = mysqli_fetch_array($stores)) 
+{
+	$temp = 341 * 12 * singleDayForecastGen($textDateRange, 3066, $row['beerstore_store_id'], 'Bottle', 12, 341);
+	echo 'this stores volume: ' . $temp . '</br>';
+
+	$total = $total + $temp;
+	echo 'running total is:' . $total . '</br> ';
+}
+echo $total;
 
 // echo ' next 3 days forcasted values: </br>';
 
