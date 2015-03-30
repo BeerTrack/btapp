9a90ee0a85d167bfd94af99ecb9c9c19abf99b06
Phil Everson <phil@phileverson.com>
2015-03-30 11:14:56 -0400

    Removed the unused fields from the manage beer section.

--- app/global/manage-beers/_viewAllBeer.php
+++ app/global/manage-beers/_viewAllBeer.php
@@ -15,9 +15,9 @@
 			<thead>
 				<tr>
 					<th>Beer Name</th>
-					<th>Beer Price</th>
-					<th>Beer Size</th>
-					<th>Beer Type</th>
+					<!-- <th>Beer Price</th> -->
+					<!-- <th>Beer Size</th> -->
+					<!-- <th>Beer Type</th> -->
 					<th>Options</th>
 				</tr>
 			</thead>
@@ -28,9 +28,9 @@
 				while($row = mysqli_fetch_array($displayTransactionQuery)) {
 					echo "<tr>";
 					echo "<td>" . $row['beer_name'] . "</td>";
-					echo "<td>" . $row['beer_price'] . "</td>";
-					echo "<td>" . $row['beer_size'] . "</td>";
-					echo "<td>" . $row['beer_type'] . "</td>";
+					// echo "<td>" . $row['beer_price'] . "</td>";
+					// echo "<td>" . $row['beer_size'] . "</td>";
+					// echo "<td>" . $row['beer_type'] . "</td>";
 					echo "<td class=\"options-align-right\"> <a href=\"?viewName=edit&beerId=" . $row['beer_id'] . "\"><button class=\"btn btn-xs btn-primary\">Edit Beer</button></a></td>";
 					echo "</tr>";
 				}
